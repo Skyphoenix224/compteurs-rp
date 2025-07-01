f028161f284c76f67ebeeadb3ee54155246cebe4
Skyphoenix224 <ygv25@outlook.fr>
2025-07-01 10:21:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,22 @@
-const CACHE_NAME = "compteurs-rp-cache-v1";
-const URLS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-// Installe le service worker et met en cache les fichiers
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
-});
-
-// Active le service worker et supprime les anciens caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) return caches.delete(name);
-        })
-      )
+    caches.open("compteurs-cache").then((cache) =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ])
     )
   );
+  self.skipWaiting();
 });
 
-// Intercepte les requêtes réseau et renvoie les fichiers depuis le cache
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(
+      (response) => response || fetch(event.request)
+    )
   );
 });
